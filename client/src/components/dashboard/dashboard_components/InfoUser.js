975fe7a71784d02eaf70f07fe357104679dc9005
gabriel-1091 <gabi@1091.com>
2024-01-10 16:04:59 +0200

    student dashboard redesign

--- client/src/components/dashboard/dashboard_components/InfoUser.js
+++ client/src/components/dashboard/dashboard_components/InfoUser.js
@@ -4,19 +4,36 @@ import React from 'react';
 const UserInfo = ({ username, id, onLogout }) => {
   const userInfoStyle = {
     position: 'fixed',
-    top: '10px',
-    right: '10px',
+    top: '0',
+    left: '0',
+    width: '100%',
     padding: '10px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    backgroundColor: '#fff',
+    background: '#f8f8f8',
+    borderBottom: '1px solid #ccc',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  
+  };
+
+  const userInfoTextStyle = {
+    margin: '0',
+  };
+
+  const logoutButtonStyle = {
+    cursor: 'pointer',
+    border: 'none',
+    background: 'none',
+    color: 'blue',
+    textDecoration: 'underline',
   };
 
   return (
     <div style={userInfoStyle}>
-      <p>Logged in as: {username}</p>
-      <p>ID: {id}</p>
-      <button onClick={onLogout}>Logout</button>
+      <p style={userInfoTextStyle}>Logged in as: {username} ----- ID: {id}</p>
+      <button style={logoutButtonStyle} onClick={onLogout}>
+        Logout
+      </button>
     </div>
   );
 };
--- client/src/components/dashboard/dashboard_components/ProjectList.js
+++ client/src/components/dashboard/dashboard_components/ProjectList.js
@@ -30,35 +30,66 @@ const ProjectList = () => {
 
   return (
     <div className="page-container">
-      <div className="project-list-container">
-        <h2>Complete project list:</h2>
-        {projects.map((project) => (
-          <div key={project.ProiectID} className="project-item">
-            <div className="project-info">
-              <Link to="/" onClick={() => handleClick(project.ProiectID)}>
-                <h3>{project.NumeProiect}</h3>
-              </Link>
-              <p>{project.Descriere}</p>
-              <p>Nota proiect: {project.NotaProiect}</p>
+      <div className="left-container">
+        <div className="project-list-container">
+          <h2>Complete project list:</h2>
+          {projects.map((project) => (
+            <div key={project.ProiectID} className="project-item">
+              <div className="project-info">
+                <Link to="/project-info" onClick={() => handleClick(project.ProiectID)}>
+                  <h3>{project.NumeProiect}</h3>
+                </Link>
+                <p>{project.Descriere}</p>
+                <p>Nota proiect: {project.NotaProiect}</p>
+              </div>
+              <span className={`project-icon ${project.IconitaProiect === 2 ? 'add' : ''}`}>
+                {getIconEmoji(project.IconitaProiect)}
+              </span>
             </div>
-            <span className={`project-icon ${project.IconitaProiect === 2 ? 'add' : ''}`}>
-              {getIconEmoji(project.IconitaProiect)}
-            </span>
-          </div>
-        ))}
+          ))}
+        </div>
+
+       
       </div>
 
+      <div className="right-container">
       <div className="part-of-projects-container">
-        <h2>You are part of the following projects:</h2>
-        {/* Render the list of projects the user is part of */}
-        {/* [project 1] [project 1 description] and so on */}
-        {/* You can replace the following placeholder text with your actual data */}
-        <div className="part-of-project-item">
-          [Project 1]
-          <br />
-          [Project 1 description] and so on
+          <h2>You are part of the following projects:</h2>
+          {/* Render the list of projects the user is part of */}
+          {/* [project 1] [project 1 description] and so on */}
+          {/* You can replace the following placeholder text with your actual data */}
+          <div className="part-of-project-item">
+            [Project 1]
+            <br />
+            [Project 1 description] and so on
+          </div>
+          {/* Repeat the above structure for each project the user is part of */}
         </div>
-        {/* Repeat the above structure for each project the user is part of */}
+        <div className="jury-projects-container">
+          <h2>You have been selected jury for the following projects:</h2>
+          {/* Render the list of projects the user is selected as a jury for */}
+          {/* [Proiect 3] [Descriere proiect 3] and so on */}
+          {/* You can replace the following placeholder text with your actual data */}
+          <div className="jury-project-item">
+            [Proiect 3]
+            <br />
+            [Descriere proiect 3] and so on
+          </div>
+          {/* Repeat the above structure for each project the user is selected as a jury for */}
+        </div>
+
+        <div className="jury-chief-projects-container">
+          <h2>You are chief juror for the following projects:</h2>
+          {/* Render the list of projects the user is chief juror for */}
+          {/* [Proiect 4] [Descriere proiect 4] and so on */}
+          {/* You can replace the following placeholder text with your actual data */}
+          <div className="jury-chief-project-item">
+            [Proiect 4]
+            <br />
+            [Descriere proiect 4] and so on
+          </div>
+          {/* Repeat the above structure for each project the user is chief juror for */}
+        </div>
       </div>
     </div>
   );
